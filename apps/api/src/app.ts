f7e2ea7e70000b9e149865ad722094e8cface277
Edison Chan <edisonchanwk@gmail.com>
2023-08-15 02:44:14 +0800

    feat: handle 404

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,7 @@
 import morgan from "morgan";
 import express from "express";
+import httpError from "http-errors";
+import httpStatus from "http-status";
 
 import { config } from "@/shared/config";
 import { errorHandler } from "@/shared/middlewares";
@@ -19,6 +21,9 @@ export function main() {
     console.log(`Server is running on port ${config.server.port}`);
   });
 
+  app.use((req, res) => {
+    throw httpError(httpStatus.NOT_FOUND, "Not Found");
+  });
   app.use(errorHandler);
 
   return app;
