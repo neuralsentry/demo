e2a7154c4382ece4b9c47fd42552235cf1473183
Edison Chan <edisonchanwk@gmail.com>
2023-08-16 21:55:55 +0800

    feat: reduce rate limit max req

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -11,7 +11,7 @@ import { routes } from "./routes";
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
   statusCode: httpStatus.TOO_MANY_REQUESTS,
