d71d5ba841166f27c5e59c237959328d185564ba
Edison Chan <edisonchanwk@gmail.com>
2023-08-17 17:55:59 +0800

    feat: responsive home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main className="flex flex-col items-center mb-52 md:mb-32 2xl:mb-12 px-2">
       <header className="flex flex-col items-center text-center mt-[100px]">
-        <h1 className="mt-10 text-6xl font-bold">
+        <h1 className="text-5xl md:text-6xl font-bold max-w-[500px] lg:max-w-full">
           Are you up for a <span className="text-secondary">challenge</span>?
         </h1>
-        <p className="py-6 max-w-[60ch] mt-2">
+        <p className="py-6 mt-2 max-w-[40ch] md:max-w-[50ch] lg:max-w-[60ch]">
           Test your skills and see if you can detect more vulnerable C/C++
           functions than our Artificial Intelligence (AI) model.
         </p>
         <div className="mt-10 gap-x-5">
           <Link href="/challenge">
-            <button className="btn btn-secondary btn-wide">Yes</button>
+            <button className="btn  btn-secondary btn-wide">Yes</button>
           </Link>
           <div className="divider">or</div>
           <Link href="/about">
